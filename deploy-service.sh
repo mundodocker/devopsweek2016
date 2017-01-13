47122809ce04416ec0ef34928f5abbe2b6e251ba
cassio <cassio.bock@gmail.com>
2017-01-13 11:35:59 +0000

    updated respository name

--- deploy-service.sh
+++ deploy-service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker pull bockmundodocker/tchelinux
-docker service update --container-label-add deploy=$(date -u +%Y-%m-%dT%H:%M:%S) --image bockmundodocker/tchelinux tchelinux
+docker pull mundodocker/devopsweek
+docker service update --container-label-add deploy=$(date -u +%Y-%m-%dT%H:%M:%S) --image mundodocker/devopsweek devopsweek
